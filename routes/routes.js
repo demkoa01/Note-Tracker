ea3ece06305252b2c5df47ee73b4ee9f560f6042
Allison Demkovich <demkovich.allison@gmail.com>
2022-02-17 20:05:35 -0600

    set up API and HTML routes in routes.js file

---
+++ routes/routes.js
@@ -0,0 +1,18 @@
+// require dependencies
+const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+
+// HTML routes
+// get notes & return notes.html
+router.get
+
+// return index.html
+router.get
+
+// API routes
+// get notes read from db.json file and return saved notes as JSON
+router.get
+
+// post -> receive new note and add to db.json
+router.post
